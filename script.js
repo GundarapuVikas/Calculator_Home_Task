d4168c6298dfc226e6dfebce48205ba3f64a0fd3
LAPTOP-88O31HJ4\vikas <vikasgundarapu@gmail.com>
2023-01-19 18:59:05 +0530

    one single method for operations

--- script.js
+++ script.js
@@ -4,48 +4,30 @@ function store(a, b, op, res) {
     localStorage.setItem("operation", op);
     localStorage.setItem("result", res);
 }
-function show(){
-    op=localStorage.getItem("operation")
-    var operation="";
+function show(a,b,op,res){
+    let operation="";
     switch(op){
         case '+': operation="sum"; break;
         case '-': operation="difference"; break;
         case '*': operation="product"; break;
         case '/': operation="division"; break;
     }
-    document.getElementById("desp").innerHTML="😀: The "+operation+" of "+localStorage.getItem("first_number")+" and "+localStorage.getItem("second_number")+" is "+localStorage.getItem("result")+" 🎉";
+    document.getElementById("desp").innerHTML="😀: The "+operation+" of "+a+" and "+b+" is "+res+" 🎉";
 }
-function add() {
-    let a = document.getElementById("a").value;
-    let b = document.getElementById("b").value;
-    let temp = Number(a) + Number(b);
-    document.getElementById("res").value = temp;
-    store(a, b, '+', temp);
-    show();
-}
-function diff() {
-    let a = document.getElementById("a").value;
-    let b = document.getElementById("b").value;
-    let temp = Number(a) - Number(b);
-    document.getElementById("res").value = temp;
-    store(a, b, '-', temp);
-    show();
-}
-function product() {
-    let a = document.getElementById("a").value;
-    let b = document.getElementById("b").value;
-    let temp = Number(a) * Number(b);
-    document.getElementById("res").value = temp;
-    store(a, b, '*', temp);
-    show();
-}
-function divide() {
-    let a = document.getElementById("a").value;
-    let b = document.getElementById("b").value;
-    let temp = Number(a) / Number(b);
-    document.getElementById("res").value = temp;
-    store(a, b, '/', temp);
-    show();
+function solve(op){
+    let a = Number(document.getElementById("a").value);
+    let b = Number(document.getElementById("b").value);
+    let res=0;
+    switch(op){
+        case '+': res=a+b;break;
+        case '-': res=a-b;break;
+        case '*': res=a*b;break;
+        case '/': res=a/b;break;
+    }
+    console.log(res);
+    // document.getElementById("result").value=res;
+    store(a,b,op,res);
+    show(a,b,op,res);
 }
 function clr() {
     if(document.getElementById("a").value!="" && document.getElementById("b").value!="" && document.getElementById("res").value!=""){
@@ -53,12 +35,12 @@ function clr() {
         document.getElementById("b").value = "";
         document.getElementById("res").value = "";
         document.getElementById("desp").innerHTML="Perform any Operation☹️..."
-        var table=document.getElementById("hist_table");
-        var row=table.insertRow(1);
-        var cell1=row.insertCell(0);
-        var cell2=row.insertCell(1);
-        var cell3=row.insertCell(2);
-        var cell4=row.insertCell(3);
+        let table=document.getElementById("hist_table");
+        let row=table.insertRow(1);
+        let cell1=row.insertCell(0);
+        let cell2=row.insertCell(1);
+        let cell3=row.insertCell(2);
+        let cell4=row.insertCell(3);
         cell1.innerHTML=localStorage.getItem("first_number");
         cell2.innerHTML=localStorage.getItem("operation");
         cell3.innerHTML=localStorage.getItem("second_number");
